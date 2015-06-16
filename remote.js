64fc250b73077747c8a82e421076d7381c573014
kumavis <aaron@kumavis.me>
2015-06-16 16:44:20 -0700

    use secureTrie as base for remoteTrie

--- remote.js
+++ remote.js
@@ -1,5 +1,6 @@
 const StateTrie = require('merkle-patricia-tree')
 const inherits = require('util').inherits
+const secureInterface = require('merkle-patricia-tree/secure-interface.js')
 const remoteInterface = require('./remote-interface.js')
 module.exports = RemoteTrie
 
@@ -9,4 +10,5 @@ inherits(RemoteTrie, StateTrie)
 function RemoteTrie(opts) {
   StateTrie.call(this, opts)
   remoteInterface(this)
+  secureInterface(this)
 }
