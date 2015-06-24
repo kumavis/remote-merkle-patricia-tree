ba24ce57ec29bf06c90df90bfa624e2418e0be78
kumavis <aaron@kumavis.me>
2015-06-23 18:45:21 -0700

    test - slightly more formal tests

--- test/index.js
+++ test/index.js
@@ -1,31 +1,54 @@
+const test = require('tape')
 const RemoteTrie = require('../remote')
 const HostTrie = require('../host')
 
-var remote = new RemoteTrie()
-var host = new HostTrie()
+test('basic functionality', function(t){
+  t.plan(6)
 
-var transport = remote.createNetworkStream()
-transport.pipe(host.createNetworkStream()).pipe(transport)
+  var remote = new RemoteTrie()
+  var host = new HostTrie()
+  var transport = remote.createNetworkStream()
+  transport.pipe(host.createNetworkStream()).pipe(transport)
 
-console.log('starting test...')
-
-remote.checkpoint()
-remote.put('beans', 'cakes', function(){
-  console.log('remote.put -', remote.root.toString('hex'))
-  console.log('host.put -', host.root.toString('hex'))
-  remote.get('beans', function(err, result){
-    console.log('remote.get -', remote.root.toString('hex'))
-    console.log('host.get -', host.root.toString('hex'))
-    console.log(result.toString())
-    remote.revert(function(){
-      console.log('remote.revert -', remote.root.toString('hex'))
-      console.log('host.revert -', host.root.toString('hex'))
-      remote.get('beans', function(err, result){
-        console.log('remote.get -', remote.root.toString('hex'))
-        console.log('host.get -', host.root.toString('hex'))
-        console.log(result && result.toString())
-        process.exit()
+  remote.checkpoint()
+  remote.put('beans', 'cakes', function(){
+    t.equals(remote.root.toString('hex'), host.root.toString('hex'), 'roots match')
+    remote.get('beans', function(err, result){
+      t.equals(remote.root.toString('hex'), host.root.toString('hex'), 'roots match')
+      t.equals(result.toString(), 'cakes', 'value should be as set, "cakes"')
+      remote.revert(function(){
+        t.equals(remote.root.toString('hex'), host.root.toString('hex'), 'roots match')
+        remote.get('beans', function(err, result){
+          t.equals(remote.root.toString('hex'), host.root.toString('hex'), 'roots match')
+          t.equals(result, null, 'value should be null')
+        })
       })
     })
   })
+
+})
+
+test('root override', function(t){
+  t.plan(3)
+
+  var remote = new RemoteTrie()
+  var host = new HostTrie()
+  var transport = remote.createNetworkStream()
+  transport.pipe(host.createNetworkStream()).pipe(transport)
+
+  remote.put('beans', 'cakes', function(){
+    var oldRoot = remote.root
+    remote.put('beans', 'gross', function(){
+      remote.get('beans', function(err, result){
+        t.equals(result.toString(), 'gross', 'value should be as overwritten, "gross"')
+        remote.root = oldRoot
+        // need to perform another operation before roots sync
+        remote.get('beans', function(err, result){
+          t.equals(result.toString(), 'cakes', 'value should be as originally set, "cakes"')
+          t.equals(remote.root.toString('hex'), host.root.toString('hex'), 'roots match')
+        })
+      })
+    })
+  })
+
 })
